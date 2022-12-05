0c4fed2cfb7b8e8747297bb0ae6262fc32e5dea3
mattbbc <55093072+mattbbc@users.noreply.github.com>
2022-12-05 12:34:00 +0000

    Add tests

---
+++ components/TestComponent.spec.tsx
@@ -0,0 +1,15 @@
+import { render } from '@testing-library/react';
+import TestComponent from './TestComponent';
+
+describe('Test component', () => {
+  let findByTestId;
+
+  beforeEach(() => {
+    const component = render(<TestComponent />);
+    findByTestId = component.findByTestId;
+  });
+
+  it('should do something', () => {
+    expect(1).toEqual(1);
+  });
+});
---
+++ components/TestComponent.tsx
@@ -0,0 +1,7 @@
+export default function TestComponent() {
+  return (
+    <>
+      <p test-id="test_id">This is a test component.</p>
+    </>
+  );
+}
